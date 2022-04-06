9da697d1cb0a65180ea3e06fc131647cf3e2996c
Aoi <ht.so.ik0102@gmail.com>
2022-04-07 06:42:32 +0700

    Add instructions directory

---
+++ anchor-escrow/programs/anchor-escrow/src/instructions/mod.rs
@@ -0,0 +1,4 @@
+pub use exchange::*;
+pub use init_escrow::*;
+
+pub mod exchange;pub mod init_escrow;
--- anchor-escrow/programs/anchor-escrow/src/lib.rs
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -1,15 +1,17 @@
 use anchor_lang::prelude::*;
 
+pub mod error;
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod anchor_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initiEscrow(ctx: Context<InitEscrow>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitEscrow<'info> {}
