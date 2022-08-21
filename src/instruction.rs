37e7c041291addc2136bf029ad3692f5efc30697
Aoi <ht.so.ik0102@gmail.com>
2022-08-22 06:33:22 +0700

    Update

--- src/instruction.rs
+++ src/instruction.rs
@@ -47,6 +47,9 @@ impl EscrowInstruction {
             0 => Self::InitEscrow {
                 amount: Self::unpack_amount(rest)?,
             },
+            1 => Self::Exchange {
+                amount: Self::unpack_amount(rest)?,
+            },
             _ => return Err(InvalidInstruction.into()),
         })
     }
