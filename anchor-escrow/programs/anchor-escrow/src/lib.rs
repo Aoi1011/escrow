f5056403026b61976c32cc8a09eb86a0bbaf3c2d
Aoi <ht.so.ik0102@gmail.com>
2022-08-28 16:03:58 +0700

    commit

--- anchor-escrow/programs/anchor-escrow/src/lib.rs
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -37,7 +37,7 @@ pub mod anchor_escrow {
         let (vault_authority, _vault_authority_bump) =
             Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
         token::set_authority(
-            ctx.accounts.intk_set_authority_context(),
+            ctx.accounts.init_set_authority_context(),
             AuthorityType::AccountOwner,
             Some(vault_authority),
         )?;
@@ -52,7 +52,7 @@ pub mod anchor_escrow {
 
         token::transfer(
             ctx.accounts
-                .into_transfer_to_taker_context()
+                .into_transfer_to_initializer_context()
                 .with_signer(&[&authority_seeds[..]]),
             ctx.accounts.escrow_account.initializer_amount,
         )?;
@@ -94,16 +94,90 @@ pub mod anchor_escrow {
 }
 
 #[derive(Accounts)]
+#[instruction(vault_account_bump: u8, initializer_amount: u64)]
 pub struct Initialize<'info> {
+    #[account(mut, signer)]
     pub initializer: AccountInfo<'info>,
     pub mint: Account<'info, Mint>,
+    #[account(
+        init, 
+        seeds = [b"token-seed".as_ref()], 
+        bump, 
+        payer = initializer, 
+        token::mint = mint, 
+        token::authority = initializer
+    )]
     pub vault_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        constraint = initializer_deposit_token_account.amount >= initializer_amount
+    )]
     pub initializer_deposit_token_account: Account<'info, TokenAccount>,
     pub initializer_receive_token_account: Account<'info, TokenAccount>,
+    #[account(zero)]
     pub escrow_account: Box<Account<'info, EscrowAccount>>,
     pub system_program: AccountInfo<'info>,
     pub rent: Sysvar<'info, Rent>,
     pub token_program: AccountInfo<'info>,
+}
+
+
+#[derive(Accounts)]
+pub struct Cancel<'info> {
+    #[account(mut, signer)]
+    pub initializer: AccountInfo<'info>,
+    #[account(mut)]
+    pub vault_account: Account<'info, TokenAccount>,
+    pub vault_authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub initializer_deposit_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        constraint = escrow_account.initializer_key == *initializer.key,
+        constraint = escrow_account.initializer_deposit_token_account == *initializer_deposit_token_account.to_account_info().key,
+        close = initializer    
+    )]
+    pub escrow_account: Box<Account<'info, EscrowAccount>>,
+    pub token_program: AccountInfo<'info>,
+}
+
+
+#[derive(Accounts)]
+pub struct Exchange<'info> {
+    #[account(signer)]
+    pub taker: AccountInfo<'info>,
+    #[account(mut)]
+    pub taker_deposit_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub taker_receive_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub initializer_deposit_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub initializer_receive_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub initializer: AccountInfo<'info>,
+    #[account(
+        mut,
+        constraint = escrow_account.taker_amount == taker_deposit_token_account.amount, 
+        constraint = escrow_account.initializer_deposit_token_account == *initializer_deposit_token_account.to_account_info().key,
+        constraint = escrow_account.initializer_receive_token_account == *initializer_receive_token_account.to_account_info().key,
+        constraint = escrow_account.initializer_key == *initializer.key,
+        close = initializer    
+    )]
+    pub escrow_account: Box<Account<'info, EscrowAccount>>,
+    #[account(mut)]
+    pub vault_account: Account<'info, TokenAccount>,
+    pub vault_authority: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+}
+
+#[account]
+pub struct EscrowAccount {
+    pub initializer_key: Pubkey,
+    pub initializer_deposit_token_account: Pubkey,
+    pub initializer_receive_token_account: Pubkey,
+    pub initializer_amount: u64,
+    pub taker_amount: u64,
 }
 
 impl<'info> Initialize<'info> {
@@ -122,23 +196,13 @@ impl<'info> Initialize<'info> {
         CpiContext::new(self.token_program.clone(), cpi_accounts)
     }
 
-    fn into_set_authority(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+    fn init_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
         let cpi_accounts = SetAuthority {
             account_or_mint: self.vault_account.to_account_info().clone(),
             current_authority: self.initializer.clone(),
         };
         CpiContext::new(self.token_program.clone(), cpi_accounts)
     }
-}
-
-#[derive(Accounts)]
-pub struct Cancel<'info> {
-    pub initializer: AccountInfo<'info>,
-    pub initializer_deposit_token_account: Account<'info, TokenAccount>,
-    pub vault_account: Account<'info, TokenAccount>,
-    pub vault_authority: AccountInfo<'info>,
-    pub escrow_account: Box<Account<'info, EscrowAccount>>,
-    pub token_program: AccountInfo<'info>,
 }
 
 impl<'info> Cancel<'info> {
@@ -164,20 +228,6 @@ impl<'info> Cancel<'info> {
         };
         CpiContext::new(self.token_program.clone(), cpi_accounts)
     }
-}
-
-#[derive(Accounts)]
-pub struct Exchange<'info> {
-    pub taker: AccountInfo<'info>,
-    pub taker_deposit_token_account: Account<'info, TokenAccount>,
-    pub taker_receive_token_account: Account<'info, TokenAccount>,
-    pub initializer_deposit_token_account: Account<'info, TokenAccount>,
-    pub initializer_receive_token_account: Account<'info, TokenAccount>,
-    pub initializer: AccountInfo<'info>,
-    pub escrow_account: Box<Account<'info, EscrowAccount>>,
-    pub vault_account: Account<'info, TokenAccount>,
-    pub vault_authority: AccountInfo<'info>,
-    pub token_program: AccountInfo<'info>,
 }
 
 impl<'info> Exchange<'info> {
@@ -217,11 +267,3 @@ impl<'info> Exchange<'info> {
     }
 }
 
-#[account]
-pub struct EscrowAccount {
-    pub initializer_key: Pubkey,
-    pub initializer_deposit_token_account: Pubkey,
-    pub initializer_receive_token_account: Pubkey,
-    pub initializer_amount: u64,
-    pub taker_amount: u64,
-}
