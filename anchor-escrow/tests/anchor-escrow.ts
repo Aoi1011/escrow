7ffb79cdd1df593cb270935c015700224f7b4edc
Aoi <ht.so.ik0102@gmail.com>
2022-08-28 21:26:13 +0700

    Start writing test code

--- anchor-escrow/tests/anchor-escrow.ts
+++ anchor-escrow/tests/anchor-escrow.ts
@@ -1,14 +1,56 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { AnchorEscrow } from "../target/types/anchor_escrow";
+import {PublicKey, SystemProgram, Transaction} from "@solana/web3.js";
+import {TOKEN_PROGRAM_ID, Token} from "@solana/spl-token";
+import {assert} from "chai";
 
 describe("anchor-escrow", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.AnchorEscrow as Program<AnchorEscrow>;
 
-  it("Is initialized!", async () => {
+  let mintA = null;
+  let mintB = null;
+  let initializerTokenAccountA = null;
+  let initializerTokenAccountB = null;
+  let takerTokenAccountA = null;
+  let takerTokenAccountB = null;
+  let vault_account_pda = null;
+  let vault_account_bump = null;
+  let vault_authority_pda = null;
+  
+  const takerAmount = 1000;
+  const initializerAmount = 500;
+  
+  const escrowAccount = anchor.web3.Keypair.generate();
+  const payer = anchor.web3.Keypair.generate();
+  const mintAuthority = anchor.web3.Keypair.generate();
+  const initializerMainAccount = anchor.web3.Keypair.generate();
+  const takerMainAccount = anchor.web3.Keypair.generate();
+  
+  
+  it("Initialize program state", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Initialize escrow", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Exchange escrow state", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Initialize escrow and cancel escrow", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
