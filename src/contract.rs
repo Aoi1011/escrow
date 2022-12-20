7770276bd59f812fa3d84180ee9a4d4323ae8409
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-21 00:00:51 +0900

    Modify contract.rs

--- src/contract.rs
+++ src/contract.rs
@@ -1,10 +1,12 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{
+    to_binary, Addr, BankMsg, Binary, Coin, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+};
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::msg::{ArbiterResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{Config, CONFIG};
 
 const CONTRACT_NAME: &str = "crates.io:escrow";
@@ -38,17 +40,84 @@ pub fn instantiate(
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: ExecuteMsg,
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    match msg {
+        ExecuteMsg::Approve { quantity } => execute_approve(deps, env, info, quantity),
+        ExecuteMsg::Refund {} => execute_refund(deps, env, info),
+    }
+}
+
+fn execute_approve(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    quantity: Option<Vec<Coin>>,
+) -> Result<Response, ContractError> {
+    let config = CONFIG.load(deps.storage)?;
+    if info.sender != config.arbiter {
+        return Err(ContractError::Unauthorized {});
+    }
+
+    // throws error if the contract is expired
+    if let Some(expiration) = config.expiration {
+        if expiration.is_expired(&env.block) {
+            return Err(ContractError::Expired { expiration });
+        }
+    }
+
+    let amount = if let Some(quantity) = quantity {
+        quantity
+    } else {
+        // release everything
+        // Querier guarantees to return up-to-date data. including funds sent in this handle message
+        deps.querier.query_all_balances(&env.contract.address)?
+    };
+
+    Ok(send_tokens(config.recipient, amount, "approve"))
+}
+
+fn execute_refund(deps: DepsMut, env: Env, _info: MessageInfo) -> Result<Response, ContractError> {
+    let config = CONFIG.load(deps.storage)?;
+
+    // throws error if the contract is expired
+    if let Some(expiration) = config.expiration {
+        if !expiration.is_expired(&env.block) {
+            return Err(ContractError::NotExpired {});
+        }
+    } else {
+        return Err(ContractError::NotExpired {});
+    }
+
+    let balance = deps.querier.query_all_balances(env.contract.address)?;
+
+    Ok(send_tokens(config.arbiter, balance, "refund"))
+}
+
+fn send_tokens(to_address: Addr, amount: Vec<Coin>, action: &str) -> Response {
+    Response::new()
+        .add_message(BankMsg::Send {
+            to_address: to_address.clone().into(),
+            amount,
+        })
+        .add_attribute("action", action)
+        .add_attribute("to", to_address)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
-    unimplemented!()
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::Arbiter {} => to_binary(&query_arbiter(deps)?),
+    }
+}
+
+fn query_arbiter(deps: Deps) -> StdResult<ArbiterResponse> {
+    let config = CONFIG.load(deps.storage)?;
+    let addr = config.arbiter;
+    Ok(ArbiterResponse { arbiter: addr })
 }
 
 #[cfg(test)]
